3c856996496528426ebd07d8a151eaeb43470b5c
Dietrich Epp <depp@zdome.net>
2022-05-27 06:59:24 -0400

    Add conversion from extended to float64

--- extended.go
+++ extended.go
@@ -57,3 +57,65 @@ func FromFloat64(x float64) (e Extended) {
 		return Extended{uint16(sign | exponent), 1<<63 | mantissa<<11}
 	}
 }
+
+// Float64 returns the value of this 80-bit floating-point number as a float64.
+// The result is rounded to the nearest float64, breaking ties towards even in
+// the least-significant bit. Values which, after rounding, would be outside the
+// range of a float64 are flushed to zero or infinity.
+func (e Extended) Float64() float64 {
+	exponent := int(e.SignExponent) & 0x7fff
+	sign := int(e.SignExponent) & 0x8000
+	if exponent == 0x7fff {
+		if e.Fraction == 0 {
+			return math.Inf(-sign)
+		}
+		return math.Copysign(math.NaN(), float64(-sign))
+	}
+	if e.Fraction == 0 {
+		return math.Copysign(0, float64(-sign))
+	}
+	// 2^(e64 - 1023) * 1.fraction
+	// = 2^(e80 - 16383) * 1.fraction / 2^nzero
+	// e63 - 1023 = e80 - 16383
+	// e63 = e80 - 16383 + 1023 - nzero
+	nzero := bits.LeadingZeros64(e.Fraction)
+	exponent += 1023 - 16383 - nzero
+	fraction := e.Fraction << nzero
+	if exponent <= 0 {
+		// Subnormal numbers.
+		shift := 12 - exponent
+		var rem uint64
+		if shift < 64 {
+			fraction, rem = fraction>>shift, fraction<<(64-shift)
+		} else if shift == 64 {
+			fraction, rem = 0, fraction
+		} else {
+			fraction = 0
+		}
+		// The (fraction & 1) makes this round to even.
+		if rem|(fraction&1) > 1<<63 {
+			fraction++
+		}
+		exponent = 0
+	} else {
+		// Round to 52 bits. The addition of ((fraction >> 11) & 1) makes this
+		// round to even.
+		rem := fraction&((1<<11)-1) | (fraction>>11)&1
+		fraction = (fraction >> 11) & ((1 << 52) - 1)
+		if rem > 1<<10 {
+			if fraction < (1<<52)-1 {
+				fraction++
+			} else {
+				exponent++
+				fraction = 0
+			}
+		}
+		if exponent >= (1<<11)-1 {
+			return math.Inf(-sign)
+		}
+	}
+	return math.Float64frombits(
+		(uint64(e.SignExponent)&0x8000)<<48 |
+			uint64(exponent)<<52 |
+			fraction)
+}
--- extended_test.go
+++ extended_test.go
@@ -48,3 +48,63 @@ func TestFromF64(t *testing.T) {
 		})
 	}
 }
+
+func equal(x, y float64) bool {
+	return math.Float64bits(x) == math.Float64bits(y)
+}
+
+func TestToFloat64(t *testing.T) {
+	type testcase struct {
+		name     string
+		exponent uint16
+		fraction uint64
+		output   float64
+	}
+	cases := []testcase{
+		{"basic_one", 16383, 1 << 63, 1.0},
+		{"basic_two", 16384, 1 << 63, 2.0},
+		{"basic_half", 16382, 1 << 63, 0.5},
+		{"after_one", 16383, (1 << 63) + (1 << 11), 1.0000000000000002},
+		{"round_even_1", 16383, (1 << 63) + (1 << 10), 1.0},
+		{"round_even_2", 16383, (1 << 63) + (1 << 10) + 1, 1.0000000000000002},
+		{"round_even_3", 16383, (1 << 63) + (1 << 11), 1.0000000000000002},
+		{"round_even_4", 16383, (1 << 63) + (3 << 10) - 1, 1.0000000000000002},
+		{"round_even_5", 16383, (1 << 63) + (3 << 10), 1.0000000000000004},
+		{"round_exponent", 16381, ^uint64(0), 0.5},
+		{"inf", 32767, 0, math.Inf(0)},
+		{"large_1", 32000, 1 << 63, math.Inf(0)},
+		{"large_2", 32000, ^uint64(0), math.Inf(0)},
+		{"large_3", 17406, 0xfffffffffffff800, 1.7976931348623157e+308},
+		{"large_4", 17406, 0xfffffffffffffbff, 1.7976931348623157e+308},
+		{"large_5", 17406, 0xfffffffffffffc00, math.Inf(0)},
+		{"zero", 0, 0, 0.0},
+		{"nan_1", 32767, 1, math.NaN()},
+		{"nan_2", 32767, 1 << 63, math.NaN()},
+		{"smallest_normal", 15361, 1 << 63, 2.2250738585072014e-308},
+		{"subnormal", 15360, 1 << 63, 1.1125369292536007e-308},
+		{"smallest_subnormal", 15309, 1 << 63, 5e-324},
+		{"smallest_subnormal_roundup", 15308, (1 << 63) + 1, 5e-324},
+		{"smallest_subnormal_rounddown", 15308, 1 << 63, 0.0},
+		{"round_to_zero", 10000, 1 << 63, 0.0},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			for sign := 0; sign < 2; sign++ {
+				in := Extended{c.exponent, c.fraction}
+				expect := c.output
+				if sign == 1 {
+					in.SignExponent |= 0x8000
+					expect = -expect
+				}
+				out := in.Float64()
+				if !equal(out, expect) {
+					t.Errorf("%04x:%016x.Float64() = %s, expect %s",
+						in.SignExponent, in.Fraction,
+						strconv.FormatFloat(out, 'g', -1, 64),
+						strconv.FormatFloat(expect, 'g', -1, 64))
+					t.Logf("bits: %016x %016x", math.Float64bits(out), math.Float64bits(expect))
+				}
+			}
+		})
+	}
+}
